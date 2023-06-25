96f9976926836c8496b348cc27aff06485b0c035
ecmen <ecmen@ECMENERDEM>
2023-06-25 13:26:53 +0300

    Mappinge Başlandı.

---
+++ ShoppingAPI.DAL/Concrete/EntityFramework/Context/ShoppingContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingAPI.DAL.Concrete.EntityFramework.Context
+{
+    public class ShoppingContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "data source=.; initial catalog=Shopping233DB; integrated security=True; TrustServerCertificate=true");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
