84d049159a7cea274031c16c2bbb36676a9e1a14
ecmen <ecmen@ECMENERDEM>
2023-06-25 11:25:46 +0300

    Add project files.

---
+++ ShoppingAPI.DAL/Abstract/DataManagement/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingAPI.Entity.Poco;
+
+namespace ShoppingAPI.DAL.Abstract.DataManagement
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangeAsync();
+    }
+}
