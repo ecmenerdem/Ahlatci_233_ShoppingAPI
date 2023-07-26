c3a35ceea7954d1d60b95bb9af3879f7ec694a97
ecmen <ecmen@ECMENERDEM>
2023-07-26 11:48:52 +0300

    ..

--- ShoppingAPI.Api/Controllers/UserController.cs
+++ ShoppingAPI.Api/Controllers/UserController.cs
@@ -18,7 +18,6 @@ namespace ShoppingAPI.Api.Controllers
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
-        public UserController(IUserService userService, IMapper mapper)
         {
             _userService = userService;
             _mapper = mapper;
@@ -29,59 +28,26 @@ namespace ShoppingAPI.Api.Controllers
         public async Task<IActionResult> GetUsers()
         {
             var users = await _userService.GetAllAsync();
-
-            List<UserResponseDTO> userDTOList = new();
-
-            foreach (var user in users)
-            {
-                userDTOList.Add(_mapper.Map<UserResponseDTO>(user));
-
-            }
-
-            return Ok(Sonuc<List<UserResponseDTO>>.SuccessWithData(userDTOList));
         }
 
-        [HttpGet("/User/{id}")]
-        [ProducesResponseType(typeof(Sonuc<UserResponseDTO>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _userService.GetAsync(q => q.id == id);
-
-            if (user!=null)
-            {
-                UserResponseDTO userDTO = _mapper.Map<UserResponseDTO>(user);
-
-                return Ok(Sonuc<UserResponseDTO>.SuccessWithData(userDTO));
-            }
+        }
             return NotFound(Sonuc<UserResponseDTO>.SuccessNoDataFound("Kullanıcı Bulunamadı"));
            
         }
 
         //Data Transfer Object
         [HttpPost("/AddUser")]
-        [ProducesResponseType(typeof(Sonuc<UserResponseDTO>),(int)HttpStatusCode.OK)]
         public async Task<IActionResult> AddUser(UserRequestDTO userRequestDTO)
         {
-            UserRegisterValidation userRegisterValidation = new UserRegisterValidation();
-
-            if (userRegisterValidation.Validate(userRequestDTO).IsValid)
             {
-                User user = _mapper.Map<User>(userRequestDTO);
-                await _userService.AddAsync(user);
-                UserResponseDTO userResponseDTO = _mapper.Map<UserResponseDTO>(user);
-                return Ok(Sonuc<UserResponseDTO>.SuccessWithData(userResponseDTO));
-            }
-            else
+            await _userService.AddAsync(user);
             {
-                List<string> ValidatonMessages = new List<string>();
-                for (int i = 0; i < userRegisterValidation.Validate(userRequestDTO).Errors.Count; i++)
-                {
-                    ValidatonMessages.Add(userRegisterValidation.Validate(userRequestDTO).Errors[i].ErrorMessage);
-                }
-                throw new FieldValidationException(ValidatonMessages);
-            }
 
-            
+
+            return Ok(userResponseDTO);
         }
     }
 }
