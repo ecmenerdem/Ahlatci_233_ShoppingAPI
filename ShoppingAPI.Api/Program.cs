63d4fb101555bcb8c4063f94919a0bc4bfc32b56
ecmen <ecmen@ECMENERDEM>
2023-08-01 01:40:58 +0300

    .

--- ShoppingAPI.Api/Program.cs
+++ ShoppingAPI.Api/Program.cs
@@ -12,8 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy=null
-);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
