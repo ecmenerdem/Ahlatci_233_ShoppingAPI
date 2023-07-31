264133f9b8e83da19b0e12bd8a0f82051ee2bebf
ecmen <ecmen@ECMENERDEM>
2023-08-01 01:01:55 +0300

    .

---
+++ ShoppingAPI.Api/Mapping/CategoryDTOResponseMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ShoppingAPI.Entity.DTO.Category;
+using ShoppingAPI.Entity.Poco;
+
+namespace ShoppingAPI.Api.Mapping
+{
+    public class CategoryDTOResponseMapper:Profile
+    {
+        public CategoryDTOResponseMapper()
+        {
+            CreateMap<Category, CategoryDTOResponse>()
+               .ForMember(dest => dest.Name,
+               opt =>
+               {
+                   opt.MapFrom(src => src.Name);
+               }).ReverseMap();
+        }
+    }
+}
